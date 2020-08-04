c8117923fed0822b165630e9deb52c1fbdaf70e0
Diego Prestes <diego.prestes@possible.com>
2020-08-04 07:19:23 -0300

    nlw2: day1

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.css';
+
+import PageHeader from '../../components/PageHeader';
+
+function TeacherForm() {
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="That's awesome that you want to give classes." />
+    </div>
+  );
+}
+
+export default TeacherForm;
